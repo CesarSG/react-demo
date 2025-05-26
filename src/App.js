a828c39a71577768ed8e5ed2e5d40a7469d585b6
Cesar <cesar_sevilla@outlook.com>
2025-05-26 07:52:42 -0600

    Fx App: change text main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test auto deploy 
+          Deployed by Jenkins 
         </p>
         <a
           className="App-link"
